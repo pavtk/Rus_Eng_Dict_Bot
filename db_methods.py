1dec9daf4fc61c8b695f173b15ac84b1c8e0a193
cylon <cylonn@yandex.ru>
2026-03-29 20:23:56 +0500

    delete drop

--- db_methods.py
+++ db_methods.py
@@ -18,7 +18,6 @@ engine = create_async_engine(
 
 async def create_all() -> None:
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
